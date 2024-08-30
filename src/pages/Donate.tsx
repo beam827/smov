14bd6853474b21b02ab5d78b9dd4285643ec511c
michahl <79968274+michahl@users.noreply.github.com>
2024-08-30 19:50:52 +0300

    Update Donate.tsx

--- src/pages/Donate.tsx
+++ src/pages/Donate.tsx
@@ -1,9 +1,7 @@
-import React, { useState } from "react";
-import { Trans, useTranslation } from "react-i18next";
+import React from "react";
 
-import { Icon, Icons } from "@/components/Icon";
 import { ThinContainer } from "@/components/layout/ThinContainer";
-import { Heading1, Paragraph } from "@/components/utils/Text";
+import { Heading1 } from "@/components/utils/Text";
 import { PageTitle } from "@/pages/parts/util/PageTitle";
 import { conf } from "@/setup/config";
 
@@ -14,39 +12,12 @@ export function shouldHaveDmcaPage() {
 }
 
 export function DonatePage() {
-  const { t } = useTranslation();
-  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <SubPageLayout>
       <PageTitle subpage k="global.pages.dmca" />
       <ThinContainer>
         <Heading1>Donate</Heading1>
-        <Paragraph>
-          <Trans
-            i18nKey="screens.dmca.text"
-            components={{
-              bold: <span className="font-bold text-white" />,
-            }}
-            values={{
-              dmca: conf().DMCA_EMAIL,
-            }}
-          />
-        </Paragraph>
-        <Paragraph className="flex space-x-3 items-center">
-          <Icon icon={Icons.MAIL} />
-          <a
-            href={`mailto:${conf().DMCA_EMAIL}`}
-            style={{
-              transition: "color 0.3s ease",
-              color: isHovered ? "#ffffff" : "inherit",
-            }}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-          >
-            {conf().DMCA_EMAIL ?? ""}
-          </a>
-        </Paragraph>
       </ThinContainer>
     </SubPageLayout>
   );
