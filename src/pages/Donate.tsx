1270769db8736082f1f4b5bc1aa5f1879701bf49
michahl <79968274+michahl@users.noreply.github.com>
2024-08-30 19:57:38 +0300

    Update Donate.tsx

--- src/pages/Donate.tsx
+++ src/pages/Donate.tsx
@@ -3,13 +3,8 @@ import React from "react";
 import { ThinContainer } from "@/components/layout/ThinContainer";
 import { Heading1 } from "@/components/utils/Text";
 import { PageTitle } from "@/pages/parts/util/PageTitle";
-import { conf } from "@/setup/config";
 
 import { SubPageLayout } from "./layouts/SubPageLayout";
-
-export function shouldHaveDmcaPage() {
-  return !!conf().DMCA_EMAIL;
-}
 
 export function DonatePage() {
   return (
